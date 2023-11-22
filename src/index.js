13b653543350e85418040daddaa6ef7fdc888eb6
jonny <js90@hml.com>
2023-11-22 16:22:32 +0200

    test

--- src/index.js
+++ src/index.js
@@ -38,7 +38,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename="/hu_morning_W160523MR_react-master/">
         <App />
       </BrowserRouter>
     </Provider>
